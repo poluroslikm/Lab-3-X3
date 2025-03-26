1783192696547abde42c7dd1da9d0ca7acb6148b
poluroslikm <133805160+poluroslikm@users.noreply.github.com>
2025-03-26 15:34:51 +0300

    Add files via upload

---
+++ lab7.2/lab7.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cassert>
+#include <string>
+#include <cmath>
+
+//3 задание
+using namespace std;
+
+struct coord
+{
+	virtual ~coord() = default;
+	virtual double evaluate() const = 0;
+};
+struct decart : coord
+{
+	decart(coord const* x,
+
+		coord const* y) :x_(x), y_(y)
+	{
+		assert(x_ && y_);
+	}
+
+	coord const* x() const { return x_; }
+	coord const* y() const { return y_; }
+	double evaluate() const
+	{
+		cout << x << y;
+	}
+	~decart() {
+		delete x_;
+		delete y_;
+	}
+private:
+	coord const* x_;
+	coord const* y_;
+};
+
+int main()
+{
+}
